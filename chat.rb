7777bb7af6a7b25d79a4000615fcb933910f60c8
sycalvind <114632354+sycalvind@users.noreply.github.com>
2025-01-25 17:01:53 +0000

    completed

--- chat.rb
+++ chat.rb
@@ -1,1 +1,28 @@
 # Write your solution here!
+require "openai"
+require "dotenv/load"
+
+request = ""
+message_list = []
+while request != "bye"
+  if request != ""
+    client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+    message_list.append({"role" => "user", "content" => request})
+    api_response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: message_list
+      }
+    )
+    response = api_response["choices"][0]["message"]["content"]
+    puts response
+    message_list.append({"role" => "assistant", "content" => response})
+    # i did not add the response to message list at first.
+  end
+  puts "-"*50
+  puts "Hello! How can I help you today?"
+  puts "-"*50
+  request = gets.chomp
+end
+
+puts "Goodbye! Have a great day!"
